0b9d0aebe6d63116efcfee9954bee45c04d65e1f
BigF <sybigf@126.com>
2022-06-28 14:37:32 +0800

    four-layer neural network

--- train.py
+++ train.py
@@ -14,11 +14,14 @@ X = np.arange(-1*math.pi, 1*math.pi, 0.006)  # 0.006
 Y = np.sin(X)
 datasize = np.size(X)
 # creat hide parameter array
-hidesize = 10
-W1 = np.random.random((hidesize, 1))
-B1 = np.random.random((hidesize, 1))
-W2 = np.random.random((1, hidesize))
-b2 = np.random.random(1)
+hidesize1 = 10
+hidesize2 = 10
+W1 = np.random.random((hidesize1, 1))
+B1 = np.random.random((hidesize1, 1))
+W2 = np.random.random((hidesize1, hidesize2))
+B2 = np.random.random((hidesize2, 1))
+W3 = np.random.random((1, hidesize2))
+B3 = np.random.random((1, 1))
 yita = 1
 loop = 5000  # 5000
 Y_predict = np.zeros(datasize)
@@ -30,17 +33,24 @@ for loopi in range(loop):
         x = X[i]
         Z1 = W1*x + B1
         A1 = sigmoid(Z1)
-        z2 = W2*A1 + b2
-        Y_predict[i] = z2[0, 0]
+        Z2 = W2*A1 + B2
+        A2 = sigmoid(Z2)
+        Z3 = W3*A2 + B3
+        Y_predict[i] = Z3[0, 0]
         e = Y[i] - Y_predict[i]
-        dW2 = -1*A1.T
-        db2 = -1
-        dW1 = sigmoid(Z1)*(np.full_like(x, 1)-sigmoid(Z1))*x
+
+        dB3 = -1
+        dW3 = -1*A2.T
+        dB2 = sigmoid(Z2)*(1-sigmoid(Z2))
+        dW2 = sigmoid(Z2)*(1-sigmoid(Z2))*A1.T
         dB1 = sigmoid(Z1)*(np.full_like(x, 1)-sigmoid(Z1))
+        dW1 = sigmoid(Z1)*(np.full_like(x, 1)-sigmoid(Z1))*x
+        B3 = B3-dB3*yita*e
+        W3 = W3-dW3*yita*e
+        B2 = B2-dB2*yita*e
         W2 = W2-dW2*yita*e
-        b2 = b2-db2*yita*e
-        W1 = W1-dW1*yita*e
         B1 = B1-dB1*yita*e
+        W1 = W1-dW1*yita*e
 
         tempsume = tempsume + abs(e)
 
