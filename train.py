c140327037f0ae84b6b6651f101454ca81a984d0
BigF <sybigf@126.com>
2022-06-28 13:33:06 +0800

    进行数组化修改

--- train.py
+++ train.py
@@ -1,74 +1,64 @@
 import math
 import random
-from turtle import end_fill
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def sigmoid(x):
-    if x >= 0:
-        z = math.exp(-x)
-        sig = 1 / (1 + z)
-    else:
-        z = math.exp(x)
-        sig = z / (1 + z)
+    sig = np.where(x < 0, np.exp(x)/(1 + np.exp(x)), 1/(1 + np.exp(-x)))
     return sig
 
+
 # creat data set
-X = np.arange(0, 2*math.pi, 0.006)#0.006
+X = np.arange(-1*math.pi, 1*math.pi, 0.006)  # 0.006
 Y = np.sin(X)
 datasize = np.size(X)
 # creat hide parameter array
-hidesize = 5
-W1 = np.random.random((hidesize,1))
-B1 = np.random.random((hidesize,1))
-W2 = np.random.random((1,hidesize))
+hidesize = 10
+W1 = np.random.random((hidesize, 1))
+B1 = np.random.random((hidesize, 1))
+W2 = np.random.random((1, hidesize))
 b2 = np.random.random(1)
-yita = 0.005
-loop = 5000 #5000
-E = np.zeros([1,loop])
+yita = 1
+loop = 5000  # 5000
 Y_predict = np.zeros(datasize)
+LOOP = []
+E = []
 for loopi in range(loop):
     tempsume = 0
     for i in range(datasize):
         x = X[i]
-        Z1 = W1*x +B1
-        A1 = np.zeros([hidesize,1])
-        for j in range(hidesize):
-            A1[j,0] = sigmoid(Z1[j,0])
-        z2 = W2@A1 + b2
-        # a2 = sigmoid(z2[0,0])
-        Y_predict[i] = z2[0,0]
-
+        Z1 = W1*x + B1
+        A1 = sigmoid(Z1)
+        z2 = W2*A1 + b2
+        Y_predict[i] = z2[0, 0]
         e = Y[i] - Y_predict[i]
-        # dW2 = e*yita*A1
-        dW2 = np.zeros([1,hidesize])
-        for j in range(hidesize):
-            dW2[0,j] = -1*A1[j,0]
-        # db2 = -1*yita*e
+        dW2 = -1*A1.T
         db2 = -1
-        dW1 = np.zeros([hidesize,1])
-        for j in range(hidesize):
-            # dW1[j,0] = W2[0,j]*sigmoid(Z1[j,0])*(1-sigmoid(Z1[j,0]))*x*e*yita
-            dW1[j,0] = sigmoid(Z1[j,0])*(1-sigmoid(Z1[j,0]))*x
-        dB1 = np.zeros([hidesize,1])
-        for j in range(hidesize):
-            # dB1[j,0] = W2[0,j]*sigmoid(Z1[j,0])*(1-sigmoid(Z1[j,0]))*(-1)*e*yita
-            dB1[j,0] = sigmoid(Z1[j,0])*(1-sigmoid(Z1[j,0]))
+        dW1 = sigmoid(Z1)*(np.full_like(x, 1)-sigmoid(Z1))*x
+        dB1 = sigmoid(Z1)*(np.full_like(x, 1)-sigmoid(Z1))
         W2 = W2-dW2*yita*e
         b2 = b2-db2*yita*e
         W1 = W1-dW1*yita*e
         B1 = B1-dB1*yita*e
-    
+
         tempsume = tempsume + abs(e)
 
-        # print("W2",W2)
-        # print("b2",b2)
-        # print("W1",W1)
-        # print("B1",B1)
-    E[0,loopi] = tempsume
-    if loopi%5 is 0:
-        print("loop:",loopi,"\ttempsume:",tempsume)
-print("W1",W1)
-print("B1",B1)        
-print("W2",W2)
-print("b2",b2)
-
+    # if loopi % 10 is 0:
+    #     print("loop:", loopi, "\ttempsume:", tempsume)
+    LOOP.append(loopi)
+    E.append(tempsume)
+    ax1 = plt.subplot(1, 2, 1)
+    ax2 = plt.subplot(1, 2, 2)
+    plt.sca(ax1)
+    plt.plot(LOOP, E, color='black')
+    plt.sca(ax2)
+    plt.plot(X, Y)
+    plt.plot(X, Y_predict, linewidth=1, linestyle='--')
+    plt.pause(0.001)  # 暂停0.01秒
+    plt.cla()
+    plt.ioff()  # 关闭画图的窗口
+    # print("W1", W1)
+    # print("B1", B1)
+    # print("W2", W2)
+    # print("b2", b2)
